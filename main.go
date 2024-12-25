a7a89c3b14411ace459c7d5b73ba6e40346572a8
Lee In Seok <dlstjr9068@gmail.com>
2024-12-25 20:06:34 +0900

    기본 핸들러 구조 작성

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"log/slog"
-	"simple-server/database"
-	"simple-server/views"
+	"simple-server/handlers"
 
-	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/mattn/go-sqlite3"
@@ -17,33 +12,12 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-		return templ.Handler(views.Index()).Component.Render(c.Request().Context(), c.Response().Writer)
-	})
-
-	e.GET("/hello", func(c echo.Context) error {
-		return templ.Handler(views.Text("Hello")).Component.Render(c.Request().Context(), c.Response().Writer)
-	})
-
-	e.GET("/buy", func(c echo.Context) error {
-		return templ.Handler(views.Text("Buy")).Component.Render(c.Request().Context(), c.Response().Writer)
-	})
-
-	e.GET("/list", func(c echo.Context) error {
-		ctx := context.Background()
-		db, err := sql.Open("sqlite3", "file:./database/data.db")
-		if err != nil {
-			slog.Error(err.Error())
-		}
-
-		queries := database.New(db)
-		authors, err := queries.ListAuthors(ctx)
-		if err != nil {
-			slog.Error(err.Error())
-		}
-		return templ.Handler(views.Authors(authors)).Component.Render(c.Request().Context(), c.Response().Writer)
-	})
+	e.GET("/", handlers.RootHandler)
+	e.GET("/hello", handlers.HelloHandler)
+	e.GET("/buy", handlers.BuyHandler)
+	e.GET("/list", handlers.ListHanlder)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
