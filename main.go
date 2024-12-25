9e223c94332bb64c71ad61c20d1bdcfb8301deeb
Lee In Seok <dlstjr9068@gmail.com>
2024-12-25 22:04:04 +0900

    .env 설정

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"simple-server/handlers"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Failed to load .env file", "err", err)
+	}
+	slog.Info(os.Getenv("TEST_ENV"))
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
