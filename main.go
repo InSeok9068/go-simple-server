9504263ebc3402d0a0f852711b899b0328357ebf
Lee In Seok <dlstjr9068@gmail.com>
2024-12-31 11:49:33 +0900

    주석 처리

--- main.go
+++ main.go
@@ -21,19 +21,21 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Static("static"))
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	/* 페이지 렌더링 */
 	e.GET("/", handlers.RootHandler) // 페이지 렌더링
+	/* 페이지 렌더링 */
 
+	/* API */
 	e.GET("/authors", handlers.GetAuthors)     // 저자 리스트 조회
 	e.GET("/author", handlers.GetAuthor)       // 저자 조회
 	e.POST("/author", handlers.CreateAuthor)   // 저자 등록
 	e.PUT("/author", handlers.UpdateAuthor)    // 저자 수정
 	e.DELETE("/author", handlers.DeleteAuthor) // 저자 삭제
-
-	e.GET("/reset-form", handlers.ResetForm) // 저자 등록폼 리셋
+	e.GET("/reset-form", handlers.ResetForm)   // 저자 등록폼 리셋
+	/* API */
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
