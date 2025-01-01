b2b940db9558758fc9262966a0d211545b4cd601
Lee In Seok <dlstjr9068@gmail.com>
2025-01-01 16:29:06 +0900

    간단 수정

--- main.go
+++ main.go
@@ -10,19 +10,23 @@ import (
 )
 
 func main() {
+	/* 환경 설정 */
 	err := godotenv.Load()
 	if err != nil {
 		slog.Error("Failed to load .env file", "err", err)
 	}
 	slog.Info(os.Getenv("TEST_ENV"))
+	/* 환경 설정 */
 
 	e := echo.New()
 
+	/* 미들 웨어 */
 	e.Use(middleware.Static("static"))
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	/* 미들 웨어 */
 
+	/* 라우터  */
 	e.GET("/", handlers.RootHandler) // 페이지 렌더링
 
 	e.GET("/authors", handlers.GetAuthors)     // 저자 리스트 조회
@@ -32,6 +36,7 @@ func main() {
 	e.DELETE("/author", handlers.DeleteAuthor) // 저자 삭제
 
 	e.GET("/reset-form", handlers.ResetForm) // 저자 등록폼 리셋
+	/* 라우터  */
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
