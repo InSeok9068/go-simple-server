4c81413f6f2d2e6b7d8df027cf8a36e68987f9e3
Lee In Seok <dlstjr9068@gmail.com>
2025-01-04 21:33:13 +0900

    배포 확인 재검토

--- projects/sample/views/index_templ.go
+++ projects/sample/views/index_templ.go
@@ -396,7 +396,7 @@ func Index() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = Title("Go 언어로 만든 심플서버 배포 확인").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = Title("Go 언어로 만든 심플서버").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
