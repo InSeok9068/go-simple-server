22bff39b0f36a0ef5c583ec10cd7ba44f534a7b9
Lee In Seok <dlstjr9068@gmail.com>
2025-01-13 15:02:41 +0900

    데이터베이스 연결

--- projects/homepage/query.sql
+++ projects/homepage/query.sql
@@ -1,25 +1,27 @@
 -- name: GetAuthor :one
 SELECT *
 FROM authors
-WHERE id = ? LIMIT 1;
+WHERE id = ?
+LIMIT 1;
 
--- name: ListAuthors :many
+-- name: ListAuthors :many 
 SELECT *
 FROM authors
 ORDER BY name;
 
 -- name: CreateAuthor :one
 INSERT INTO authors (name, bio, created, updated)
-VALUES (?, ?, datetime('now'), datetime('now')) RETURNING *;
+VALUES (?, ?, datetime ('now'), datetime ('now'))
+RETURNING *;
 
--- name: UpdateAuthor :one
+-- name: UpdateAuthor :one 
 UPDATE authors
-set name    = ?,
-    bio     = ?,
-    updated = datetime('now')
-WHERE id = ? RETURNING *;
+set name = ?,
+  bio = ?,
+  updated = datetime ('now')
+WHERE id = ?
+RETURNING *;
 
 -- name: DeleteAuthor :exec 
-DELETE
-FROM authors
+DELETE FROM authors
 WHERE id = ?;
