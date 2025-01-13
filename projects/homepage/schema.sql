989e22886eaa5e9c79ed364b418d95120f88cfbb
Lee In Seok <dlstjr9068@gmail.com>
2025-01-13 18:19:21 +0900

    쿼리 수정

--- projects/homepage/schema.sql
+++ projects/homepage/schema.sql
@@ -1,13 +1,7 @@
-CREATE TABLE authors (
-    bio     TEXT DEFAULT ''
-                 NOT NULL,
-    created TEXT DEFAULT ''
-                 NOT NULL,
-    id      TEXT PRIMARY KEY
-                 DEFAULT ('r' || lower(hex(randomblob(7) ) ) ) 
-                 NOT NULL,
-    name    TEXT DEFAULT ''
-                 NOT NULL,
-    updated TEXT DEFAULT ''
-                 NOT NULL
-);
+CREATE    TABLE authors (
+          bio TEXT DEFAULT '' NOT NULL,
+          created TEXT DEFAULT '' NOT NULL,
+          id TEXT PRIMARY KEY DEFAULT ('r' || lower(hex (randomblob (7)))) NOT NULL,
+          name TEXT DEFAULT '' NOT NULL,
+          updated TEXT DEFAULT '' NOT NULL
+          );
