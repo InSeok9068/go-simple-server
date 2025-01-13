43197f32ef0c93b1db849509cb252738ec9f3101
Lee In Seok <dlstjr9068@gmail.com>
2025-01-13 18:17:12 +0900

    쿼리 포멧 수정

--- projects/homepage/query.sql
+++ projects/homepage/query.sql
@@ -1,27 +1,25 @@
 -- name: GetAuthor :one
-SELECT *
-FROM authors
-WHERE id = ?
-LIMIT 1;
+SELECT    *
+FROM      authors
+WHERE     id = ?
+LIMIT     1;
 
 -- name: ListAuthors :many 
-SELECT *
-FROM authors
-ORDER BY name;
+SELECT    *
+FROM      authors
+ORDER BY  name;
 
 -- name: CreateAuthor :one
-INSERT INTO authors (name, bio, created, updated)
-VALUES (?, ?, datetime ('now'), datetime ('now'))
-RETURNING *;
+INSERT    INTO authors (name, bio, created, updated)
+VALUES    (?, ?, datetime ('now'), datetime ('now')) RETURNING *;
 
 -- name: UpdateAuthor :one 
-UPDATE authors
-set name = ?,
-  bio = ?,
-  updated = datetime ('now')
-WHERE id = ?
-RETURNING *;
+UPDATE    authors
+set       name = ?,
+          bio = ?,
+          updated = datetime ('now')
+WHERE     id = ? RETURNING *;
 
 -- name: DeleteAuthor :exec 
-DELETE FROM authors
-WHERE id = ?;
+DELETE    FROM authors
+WHERE     id = ?;
