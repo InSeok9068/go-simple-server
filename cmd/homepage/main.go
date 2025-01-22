ca447abfa32b518045cf343a6dd9d84c2a1ae394
Lee In Seok <dlstjr9068@gmail.com>
2025-01-22 16:45:27 +0900

    모바일 최적화 UI 수정

--- cmd/homepage/main.go
+++ cmd/homepage/main.go
@@ -73,13 +73,13 @@ func main() {
 		return c.String(200, "Squash 실행")
 	})
 
-	private.GET("/authors", handlers.GetAuthors)     // 저자 리스트 조회
-	private.GET("/author", handlers.GetAuthor)       // 저자 조회
-	private.POST("/author", handlers.CreateAuthor)   // 저자 등록
-	private.PUT("/author", handlers.UpdateAuthor)    // 저자 수정
-	private.DELETE("/author", handlers.DeleteAuthor) // 저자 삭제
+	public.GET("/authors", handlers.GetAuthors)     // 저자 리스트 조회
+	public.GET("/author", handlers.GetAuthor)       // 저자 조회
+	public.POST("/author", handlers.CreateAuthor)   // 저자 등록
+	public.PUT("/author", handlers.UpdateAuthor)    // 저자 수정
+	public.DELETE("/author", handlers.DeleteAuthor) // 저자 삭제
 
-	private.GET("/reset-form", handlers.ResetForm) // 저자 등록폼 리셋
+	public.GET("/reset-form", handlers.ResetForm) // 저자 등록폼 리셋
 	/* 라우터  */
 
 	/* 크론 잡 */
