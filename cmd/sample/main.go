54392776d0a083fe05d20be1185ad1f0327a9fe4
Lee In Seok <dlstjr9068@gmail.com>
2025-02-07 16:28:59 +0900

    샘플 프로젝트에 라디오 버튼 기능 추가 및 레이아웃 파일 제거

---
+++ cmd/sample/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"simple-server/projects/sample/views"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return templ.Handler(views.Index("Sample")).Component.Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.GET("/radio", func(c echo.Context) error {
+		return templ.Handler(views.Radio()).Component.Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.Logger.Fatal(e.Start(":8002"))
+}
