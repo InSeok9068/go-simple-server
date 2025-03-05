6cf1aaa675f625391a4932c058b131aef7594fe5
Lee In Seok <dlstjr9068@gmail.com>
2025-03-05 14:13:18 +0900

    쿼리 포멧 추가

--- projects/sample/schema.sql
+++ projects/sample/schema.sql
@@ -1,7 +1,7 @@
-CREATE    TABLE authors (
+   CREATE TABLE authors (
           bio TEXT DEFAULT '' NOT NULL,
           created TEXT DEFAULT '' NOT NULL,
-          id TEXT PRIMARY KEY DEFAULT ('r' || lower(hex (randomblob (7)))) NOT NULL,
+          id TEXT PRIMARY KEY DEFAULT ('r' || lower(hex(randomblob(7)))) NOT NULL,
           name TEXT DEFAULT '' NOT NULL,
           updated TEXT DEFAULT '' NOT NULL
           );
