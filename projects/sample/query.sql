d7f3cd85b1168fde820ec1fd3f86cc93423646e7
Lee In Seok <dlstjr9068@gmail.com>
2025-03-21 19:00:33 +0900

    database tool 확장자 작업

--- projects/sample/query.sql
+++ projects/sample/query.sql
@@ -1,25 +1,27 @@
--- name: GetAuthor :one 
-   SELECT *
-     FROM authors
-    WHERE id = ?
-    LIMIT 1;
+-- name: GetAuthor :one
+SELECT * FROM authors WHERE id = ? LIMIT 1;
 
--- name: ListAuthors :many 
-   SELECT *
-     FROM authors
- ORDER BY name;
+-- name: ListAuthors :many
+SELECT * FROM authors ORDER BY name;
 
 -- name: CreateAuthor :one
-   INSERT INTO authors (name, bio, created, updated)
-   VALUES (?, ?, datetime('now'), datetime('now')) RETURNING *;
+INSERT INTO
+    authors (name, bio, created, updated)
+VALUES (
+        ?,
+        ?,
+        datetime('now'),
+        datetime('now')
+    ) RETURNING *;
 
--- name: UpdateAuthor :one 
-   UPDATE authors
-      set name = ?,
-          bio = ?,
-          updated = datetime('now')
-    WHERE id = ? RETURNING *;
+-- name: UpdateAuthor :one
+UPDATE authors
+set
+    name = ?,
+    bio = ?,
+    updated = datetime('now')
+WHERE
+    id = ? RETURNING *;
 
--- name: DeleteAuthor :exec 
-   DELETE FROM authors
-    WHERE id = ?;
+-- name: DeleteAuthor :exec
+DELETE FROM authors WHERE id = ?;
