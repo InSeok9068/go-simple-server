bddd7b8c8f85ee567da02abcfc184a87463a9ff6
Lee In Seok <dlstjr9068@gmail.com>
2025-06-11 14:35:09 +0900

    스키마 정의

--- projects/deario/query.sql
+++ projects/deario/query.sql
@@ -1,71 +1,81 @@
 -- sqlc generate -f ./projects/deario/sqlc.yaml
 
 -- name: GetDiary :one
-SELECT *
-FROM diarys
-WHERE date = ?
-  AND uid = ?
-LIMIT 1;
+SELECT * FROM diarys WHERE date = ? AND uid = ? LIMIT 1;
 
 -- name: GetDiaryRandom :one
 SELECT *
 FROM diarys
-WHERE date IS NOT NULL
-  AND uid = ?
+WHERE
+    date IS NOT NULL
+    AND uid = ?
 ORDER BY RANDOM()
 LIMIT 1;
 
 -- name: ListDiarys :many
 SELECT *
 FROM diarys
-WHERE uid = ?
+WHERE
+    uid = ?
 ORDER BY created desc
-LIMIT 7 OFFSET ((? - 1) * 7);
+LIMIT 7
+OFFSET ((? - 1) * 7);
 
 -- name: CreateDiary :one
-INSERT INTO diarys (uid, content, date, created, updated)
-VALUES (?,
+INSERT INTO
+    diarys (
+        uid,
+        content,
+        date,
+        created,
+        updated
+    )
+VALUES (
         ?,
         ?,
---         strftime('%Y%m%d', 'now', 'localtime'),
+        ?,
+        --         strftime('%Y%m%d', 'now', 'localtime'),
         datetime('now', 'localtime'),
-        datetime('now', 'localtime'))
-RETURNING *;
+        datetime('now', 'localtime')
+    ) RETURNING *;
 
 -- name: UpdateDiary :one
 UPDATE diarys
-SET content = ?,
+SET
+    content = ?,
     updated = datetime('now')
-WHERE id = ?
-RETURNING *;
+WHERE
+    id = ? RETURNING *;
 
 -- name: DeleteDiary :exec
-DELETE
-FROM diarys
-WHERE id = ?;
+DELETE FROM diarys WHERE id = ?;
 
 -- name: UpdateDiaryOfAiFeedback :exec
 UPDATE diarys
-SET aiFeedback = ?,
-    aiImage    = ?,
-    updated    = datetime('now')
-WHERE id = ?;
+SET
+    aiFeedback = ?,
+    aiImage = ?,
+    updated = datetime('now')
+WHERE
+    id = ?;
 
 -- name: GetPushKey :one
-SELECT *
-FROM push_keys
-WHERE uid = ?
-LIMIT 1;
+SELECT * FROM push_keys WHERE uid = ? LIMIT 1;
 
 -- name: CreatePushKey :exec
-INSERT INTO push_keys (uid, token, created, updated)
-VALUES (?,
+INSERT INTO
+    push_keys (uid, token, created, updated)
+VALUES (
+        ?,
         ?,
         datetime('now', 'localtime'),
-        datetime('now', 'localtime'));
+        datetime('now', 'localtime')
+    );
 
 -- name: UpdatePushKey :exec
 UPDATE push_keys
-SET token   = ?,
+SET
+    token = ?,
     updated = datetime('now')
-WHERE uid = ?;
+WHERE
+    uid = ?;
--- projects/deario/schema.sql
+++ projects/deario/schema.sql
@@ -1,24 +1,24 @@
 -- auto-generated definition
-create table diarys
-(
-    content    TEXT default ''                                 not null,
-    created    TEXT default ''                                 not null,
-    date       TEXT default ''                                 not null,
-    id         TEXT default ('r' || lower(hex(randomblob(7)))) not null
-        primary key,
-    uid        TEXT default ''                                 not null,
-    updated    TEXT default ''                                 not null,
-    aiFeedback TEXT default ''                                 not null,
-    aiImage    TEXT default ''                                 not null
+create table diarys (
+    id TEXT default (
+        'r' || lower(hex(randomblob(7)))
+    ) not null primary key,
+    uid TEXT default '' not null,
+    date TEXT default '' not null,
+    content TEXT default '' not null,
+    aiFeedback TEXT default '' not null,
+    aiImage TEXT default '' not null,
+    created TEXT default '' not null,
+    updated TEXT default '' not null
 );
 
 -- auto-generated definition
-create table push_keys
-(
-    created TEXT default ''                                 not null,
-    id      TEXT default ('r' || lower(hex(randomblob(7)))) not null
-        primary key,
-    token   TEXT default ''                                 not null,
-    uid     TEXT default ''                                 not null,
-    updated TEXT default ''                                 not null
+create table push_keys (
+    uid TEXT default '' not null,
+    id TEXT default (
+        'r' || lower(hex(randomblob(7)))
+    ) not null primary key,
+    token TEXT default '' not null,
+    created TEXT default '' not null,
+    updated TEXT default '' not null
 );
