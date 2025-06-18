8e989fc5ab97b41f9ffb3725fbd800bcdae43c30
Lee In Seok <dlstjr9068@gmail.com>
2025-06-18 18:20:51 +0900

    종속성 업데이트 하나의 스크립트 에서 진행

---
+++ update-deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+go get firebase.google.com/go/v4@latest
+go get github.com/Blank-Xu/sql-adapter@latest
+go get github.com/casbin/casbin/v2@latest
+go get github.com/doganarif/govisual@latest
+go get github.com/eduardolat/gomponents-lucide@latest
+go get github.com/glsubri/gomponents-alpine@latest
+go get github.com/go-rod/rod@latest
+go get github.com/gorilla/sessions@latest
+go get github.com/joho/godotenv@latest
+go get github.com/labstack/echo-contrib@latest
+go get github.com/labstack/echo/v4@latest
+go get github.com/openai/openai-go@latest
+go get github.com/robfig/cron/v3@latest
+go get github.com/willoma/bulma-gomponents@latest
+go get github.com/willoma/gomplements@latest
+go get golang.org/x/time@latest
+go get google.golang.org/api@latest
+go get google.golang.org/genai@latest
+go get maragu.dev/gomponents@latest
+go get maragu.dev/gomponents-htmx@latest
+go get modernc.org/sqlite@latest
+go install github.com/air-verse/air@latest
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
+go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
+go mod tidy
