e02bab66184bd66a829abdcdf93c6a30bd997050
Lee In Seok <dlstjr9068@gmail.com>
2025-06-18 15:38:54 +0900

    운영 오류 발생으로 인한 로깅 추가

--- projects/deario/services/users.go
+++ projects/deario/services/users.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"log/slog"
 	"net/http"
 	"simple-server/internal/middleware"
 	"simple-server/projects/deario/db"
@@ -10,26 +11,42 @@ import (
 )
 
 func EnsureUser(ctx context.Context, uid string) error {
+	slog.Info("EnsureUser called", "uid", uid)
+
 	queries, _ := db.GetQueries(ctx)
 
 	_, err := queries.GetUser(ctx, uid)
 	if err != nil {
+		slog.Info("User not found in database, creating new user", "uid", uid, "error", err)
+
 		auth, err := middleware.App.Auth(ctx)
 		if err != nil {
+			slog.Error("Failed to get auth client", "error", err)
 			return err
 		}
+
 		user, err := auth.GetUser(ctx, uid)
 		if err != nil {
+			slog.Error("Failed to get user from Firebase", "uid", uid, "error", err)
 			return echo.NewHTTPError(http.StatusUnauthorized, "유효하지 않은 사용자입니다.")
 		}
-		_ = queries.CreateUser(ctx, db.CreateUserParams{
+
+		err = queries.CreateUser(ctx, db.CreateUserParams{
 			Uid:   user.UID,
 			Name:  user.DisplayName,
 			Email: user.Email,
 		})
+		if err != nil {
+			slog.Error("Failed to create user in database", "uid", uid, "error", err)
+			return err
+		}
+		slog.Info("Successfully created new user", "uid", uid)
 	}
 
-	middleware.Enforcer.AddRoleForUser(uid, "user")
+	if _, err := middleware.Enforcer.AddRoleForUser(uid, "user"); err != nil {
+		slog.Error("Failed to add role for user", "uid", uid, "error", err)
+		return err
+	}
 
 	return nil
 }
