2c87e57ca19486bc268bf4d4733122aadfa0edb8
Lee In Seok <dlstjr9068@gmail.com>
2025-07-16 17:20:09 +0900

    저장 시 UI/UX 개선

--- projects/deario/static/deario.js
+++ projects/deario/static/deario.js
@@ -1,1 +1,15 @@
-console.log("Deario에 오신걸 환영합니다.")
+console.log("Deario에 오신걸 환영합니다.");
+
+document.addEventListener("alpine:init", () => {
+  Alpine.store("save", {
+    isOk: true,
+
+    ok() {
+      this.isOk = true;
+    },
+
+    unok() {
+      this.isOk = false;
+    },
+  });
+});
