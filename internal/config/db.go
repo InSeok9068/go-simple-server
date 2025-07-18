465c7aa879b3459c30f82bd6c11dbea4bbe90a41
Lee In Seok <dlstjr9068@gmail.com>
2025-07-18 17:36:51 +0900

    db 연결옵션 지정

--- internal/config/db.go
+++ internal/config/db.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func AppDatabaseURL(serviceName string) string {
 	if IsDevEnv() {
-		return fmt.Sprintf("file:./projects/%s/data/data.db", serviceName)
+		return fmt.Sprintf("file:./projects/%s/data/data.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)", serviceName)
 	} else {
-		return fmt.Sprintf("file:/srv/%s/data/data.db", serviceName)
+		return fmt.Sprintf("file:/srv/%s/data/data.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)", serviceName)
 	}
 }
 
 func LogDatabaseURL() string {
 	if IsDevEnv() {
-		return "file:./shared/log/auxiliary.db"
+		return "file:./shared/log/auxiliary.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)"
 	} else {
-		return "file:/srv/log/auxiliary.db"
+		return "file:/srv/log/auxiliary.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)"
 	}
 }
