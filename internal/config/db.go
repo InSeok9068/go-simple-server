707580b78890bbe8bfe473d9e556f54dd92e437d
Lee In Seok <dlstjr9068@gmail.com>
2025-07-18 18:18:12 +0900

    옵션 정리

--- internal/config/db.go
+++ internal/config/db.go
@@ -3,17 +3,30 @@ package config
 import "fmt"
 
 func AppDatabaseURL(serviceName string) string {
+	var url string
+	pragmas := `
+		&_pragma=journal_mode(WAL)
+		&_pragma=synchronous(NORMAL)
+		&_pragma=busy_timeout(5000)
+		&_pragma=foreign_keys(ON)`
 	if IsDevEnv() {
-		return fmt.Sprintf("file:./projects/%s/data/data.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)", serviceName)
+		url = fmt.Sprintf(`file:./projects/%s/data/data.db?%s`, serviceName, pragmas)
 	} else {
-		return fmt.Sprintf("file:/srv/%s/data/data.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)", serviceName)
+		url = fmt.Sprintf(`file:/srv/%s/data/data.db?%s`, serviceName, pragmas)
 	}
+	return url
 }
 
 func LogDatabaseURL() string {
+	var url string
+	pragmas := `
+		&_pragma=journal_mode(WAL)
+		&_pragma=synchronous(NORMAL)
+		&_pragma=busy_timeout(5000)`
 	if IsDevEnv() {
-		return "file:./shared/log/auxiliary.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)"
+		url = fmt.Sprintf(`file:./shared/log/auxiliary.db?%s`, pragmas)
 	} else {
-		return "file:/srv/log/auxiliary.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)"
+		url = fmt.Sprintf(`file:/srv/log/auxiliary.db?%s`, pragmas)
 	}
+	return url
 }
