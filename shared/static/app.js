52ff624a1343ce98645bf2349a112dc0ea305ad3
Lee In Seok <dlstjr9068@gmail.com>
2025-08-13 17:10:37 +0900

    폰트 선택지 추가

--- shared/static/app.js
+++ shared/static/app.js
@@ -74,11 +74,18 @@ document.addEventListener("alpine:init", () => {
     apply() {
       const fonts = {
         gamja: '"Gamja Flower"',
-        system: "system-ui",
+        humanist: "var(--font-humanist)",
+        geometric_humanist: "var(--font-geometric-humanist)",
+        classical_humanist: "var(--font-classical-humanist)",
+        neo_grotesque: "var(--font-neo-grotesque)",
+        monospace_code: "var(--font-monospace-code)",
+        industrial: "var(--font-industrial)",
+        rounded_sans: "var(--font-rounded-sans)",
+        antique: "var(--font-antique)",
       };
       document.documentElement.style.setProperty(
         "--font-family",
-        fonts[this.value] || fonts.gamja,
+        fonts[this.value] || "Gamja Flower"
       );
     },
   });
