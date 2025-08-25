48dad0ba2657537531f02d1d60860cabefb668d6
Lee In Seok <dlstjr9068@gmail.com>
2025-08-25 17:27:28 +0900

    feat: SQLite 데이터베이스 설정 최적화
    internal/config/db.go: SQLite 데이터베이스 성능 및 정합성 설정 추가

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,14 +1,21 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func AppDatabaseURL(serviceName string) string {
+	appPragmas := []string{
+		"_pragma=journal_mode(WAL)",            // 동시성 향상
+		"_pragma=synchronous(NORMAL)",          // 정합성 향상
+		"_pragma=busy_timeout(5000)",           // 쓰기 경합 대기 5초
+		"_pragma=foreign_keys(ON)",             // 외래 키 허용
+		"_pragma=temp_store(MEMORY)",           // 임시 저장소 메모리
+		"_pragma=journal_size_limit(67108864)", // 64MB
+	}
+	pragmas := strings.Join(appPragmas, "&")
 	var url string
-	pragmas := `
-		&_pragma=journal_mode(WAL)
-		&_pragma=synchronous(NORMAL)
-		&_pragma=busy_timeout(5000)
-		&_pragma=foreign_keys(ON)`
 	if IsDevEnv() {
 		url = fmt.Sprintf(`file:./projects/%s/data/data.db?%s`, serviceName, pragmas)
 	} else {
@@ -18,11 +25,15 @@ func AppDatabaseURL(serviceName string) string {
 }
 
 func LogDatabaseURL() string {
+	logPragmas := []string{
+		"_pragma=journal_mode(WAL)",            // 동시성 향상
+		"_pragma=synchronous(NORMAL)",          // 정합성 향상
+		"_pragma=busy_timeout(5000)",           // 쓰기 경합 대기 5초
+		"_pragma=temp_store(MEMORY)",           // 임시 저장소 메모리
+		"_pragma=journal_size_limit(67108864)", // 64MB
+	}
+	pragmas := strings.Join(logPragmas, "&")
 	var url string
-	pragmas := `
-		&_pragma=journal_mode(WAL)
-		&_pragma=synchronous(NORMAL)
-		&_pragma=busy_timeout(5000)`
 	if IsDevEnv() {
 		url = fmt.Sprintf(`file:./shared/log/auxiliary.db?%s`, pragmas)
 	} else {
