37c817ad6bd818bd36a9c21d034c58d4c1b3068e
Lee In Seok <dlstjr9068@gmail.com>
2025-08-25 17:28:02 +0900

    feat: SQLite 프라그마 설정 테스트
    .playground/playground.go: SQLite 데이터베이스 성능 및 동작 설정 테스트 코드 추가

--- .playground/playground.go
+++ .playground/playground.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
-	font := []string{"Roboto", "Roboto+Condensed", "Roboto+Slab"}
-	fmt.Println(strings.Join(font, "&family="))
+	appPragmas := []string{
+		"_pragma=journal_mode(WAL)",            // 동시성 향상
+		"_pragma=synchronous(NORMAL)",          // 정합성 향상
+		"_pragma=busy_timeout(5000)",           // 쓰기 경합 대기 5초
+		"_pragma=foreign_keys(ON)",             // 외래 키 허용
+		"_pragma=temp_store(MEMORY)",           // 임시 저장소 메모리
+		"_pragma=journal_size_limit(67108864)", // 64MB
+	}
+	pragmas := strings.Join(appPragmas, "&")
+	fmt.Println(pragmas)
 }
